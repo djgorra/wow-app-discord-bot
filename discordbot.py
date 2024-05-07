975d1b2d66b1c39022da3dd642f7a95218995ffb
Nicole Beguesse <nicole@monroneylabels.com>
2024-05-07 14:54:13 -0400

    bugfixes, import json

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import asyncio
 import requests
+import json
 
 
 def runBot():
@@ -19,19 +20,19 @@ def runBot():
         print("Got message: "+message.content);
         channel = message.channel
         def get_input(m):
-            return True
+            return m.author == message.author
         if message.content.startswith('<@1232040337652973652>') and "help" in message.content:
             #i.e. if the message is @Raidcraft-bot help, then send the intro message
             await channel.send('Hello! I am a bot that helps you organize your World of Warcraft runs. To start a new run, type @Raidcraft-bot $newrun in a public channel. Please create an account on https://www.raidcraft-app.com/ and get your BattleID ready. I will ask you for it in a private message.')
         elif message.content.startswith('<@1232040337652973652> $newrun'):
-            newrun_message = message
-            await newrun_message.author.send('Looks like you want to start a new run, but I don\'t have your BattleID. Please reply with your BattleID to get started.')
+            message = message
+            await message.author.send('Looks like you want to start a new run, but I don\'t have your BattleID. Please reply with your BattleID to get started.')
             battle_id_msg = await client.wait_for('message', check=get_input)
             battleID = battle_id_msg.content
-           
+            #todo: Save the discord_id and battle_id in the database, so that we don't have to ask for the Battle ID again 
             x = requests.post('https://wow-app-rails-5c78013cc11c.herokuapp.com/api/users/battle_id' , json = {'discord_id': message.author.id, 'battle_id': battleID })
             response = x.text
-            parsed = JSON.parse(response)
+            parsed = json.loads(response)
             if parsed["message"] == "User not found":
                 await message.author.send('Sorry, I couldn\'t find your account. Please create an account on https://www.raidcraft-app.com/ and try again.')
                 return
@@ -44,10 +45,8 @@ def runBot():
                     await message.author.send(i+") " + team["name"] + " - " + team["description"])
 
                 response = await client.wait_for('message', check=get_input)
-                team_id = response.content
-                # #todo: Save the discord_id and battle_id in the database, so that we don't have to ask for the Battle ID again 
-                #todo: check if the response is valid, have better error handling
-                if invite_code and team_number:
+                chosen_team = teams[int(response.content)-1]
+                if chosen_team:
                     intro_message = await channel.send('Let\'s start a new run! Reply with an emoji to join the run')
 
                     def check_reaction(reaction, user):
@@ -58,7 +57,7 @@ def runBot():
                     except asyncio.TimeoutError:
                         await channel.send('No reactions in time. 👎')
                     else:
-                        await reaction.message.author.send("Get ready to raid! Just click this link to add your character: https://www.raidcraft-app.com/characters/new?invite_code="+invite_code+"&team_id="+team_id
+                        await reaction.message.author.send("Get ready to raid! Just click this link to add your character: https://www.raidcraft-app.com/characters/new?invite_code="+chosen_team["invite_code"])
                 else:
                     await message.author.send('Sorry, something went wrong. Please try again later.')
 
